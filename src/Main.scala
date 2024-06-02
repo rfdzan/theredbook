e1387cc3ac8e65286a62570754ed96d6681d32a8
rfdzan <hekodev.py@gmail.com>
2024-06-02 17:20:20 +0700

    polymorphic functions

--- src/Main.scala
+++ src/Main.scala
@@ -1,6 +1,11 @@
 import scala.collection.mutable.ArrayBuffer
 import functionalLoops.factorialWithHof
 import MonomorphicFunc.formatResult
+import PolymorphicFunc.mainPolymorphicFunc
 @main def hello(): Unit =
-  val factorialHof = formatResult("Factorial", 5, factorialWithHof)
-  println(factorialHof)
+  // val factorialHof = formatResult("Factorial", 5, factorialWithHof)
+  // println(factorialHof)
+  mainPolymorphicFunc
+
+
+  
--- src/PolymorphicFunction.scala
+++ src/PolymorphicFunction.scala
@@ -2,3 +2,18 @@ package MonomorphicFunc:
   def formatResult(name: String, n: Int, f: Int => Int): String =
     val msg = "The %s of %d is %d"
     msg.format(name, n, f(n))
+
+package PolymorphicFunc:
+  def findFirst[A](as: Array[A], p: A => Boolean) =
+    val found = as.filter(p).toList
+    println(found)
+
+  def mainPolymorphicFunc = 
+    val nameArr= Array("bob", "john", "clara")
+    val keyword = "bob"
+
+    val intArr = Array(1,2,3,4,5)
+    val keyint = 3
+
+    findFirst(nameArr, (i) => i == keyword)
+    findFirst(intArr, (i) => i == keyint)
